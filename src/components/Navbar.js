5cc20e21cd52ba30e824b53c8091c30b315d40df
ekfman <ekfirstman@gmail.com>
2023-02-23 14:42:17 -0800

    project paragraphs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Nabvar = () => {
     return (
         <div className="navbar">
             <ul>
-                <li className="nav-link"><Link to="/"></Link>Home</li>
+                <li className="nav-link"><Link to="/home"></Link>Home</li>
                 <li className="nav-link"><Link to="/projects">Projects</Link></li>
                 <li className="nav-link"><Link to="/resume">Resume</Link></li>
                 <li className="nav-link"><Link to="contact-me">Contact Me</Link></li>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,43 @@
 import { useState } from "react";
 import journalArr from "./assets/e-journalArray";
+import "./projects.css";
 
 const Projects = () => {
-    const [digitalDiaryImg, setDigitalDiaryImg] = useState([]);
-    return (
-        <div>
-            <h2>Check out my projects.</h2>
-            <div className="project-container">
-            <img src={require("./assets/e-journal_imgs/calendarView.png")}></img>
-            <h3>Digital Diary</h3>
-            </div>
-            <div>
-                <img src={require("./assets/e-commerce_imgs/homescreen.png")}></img>
-                <h3>E-commerce Sticker Site</h3>
-            </div>
-            <div>
-                <h3>Online Secondhand Marketplace</h3>
-                <img src= {require("./assets/secondhandStore/home.png")}></img>
-            </div>
+  const [digitalDiaryImg, setDigitalDiaryImg] = useState([]);
+  return (
+    <div>
+      <h2>Check out my projects.</h2>
+      <div className="project-container">
+        <div className="projectImg-title">
+          <img src={require("./assets/e-journal_imgs/calendarView.png")}></img>
+          <h3>Confidant (Digital Diary)</h3>
+          <p>
+            I created an electronic journal called Confidant for those who find
+            writing their thoughts and experiences as therapeutic, but get tired
+            of writing or don't have time to. The fun part about this web app is
+            that users can easily find and view their past entries, even from
+            years ago.
+          </p>
         </div>
-    )
-}
+        <div className="projectImg-title">
+          <img src={require("./assets/e-commerce_imgs/homescreen.png")}></img>
+          <h3>Sticky Situations (E-commerce Sticker Site)</h3>
+          <p>
+            An online sticker storefront where users can browse and add items to a persisting cart. Registered users can view their
+            purchase history, and admins can access user information.
+          </p>
+        </div>
+        <div className="projectImg-title">
+          <img src={require("./assets/secondhandStore/home.png")}></img>
+          <h3>Online Secondhand Marketplace</h3>
+          <p>
+            Users are directed to the homepage with a view of all the listings. If registered, users can inquire about a
+            specific item, and post, edit and delete their own listings.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
