d35b1e93ee74b18a0e583b19402011a66afb73db
ekfman <ekfirstman@gmail.com>
2023-03-08 12:52:07 -0800

    added puzzle design to about page

--- src/components/About.js
+++ src/components/About.js
@@ -5,44 +5,212 @@ import { useInView } from "react-intersection-observer";
 
 const About = () => {
   const { ref: aboutContainer, inView: bioVisible } = useInView();
+  const [meHover, setMeHover] = useState(false);
+  const [frontHover, setFrontHover] = useState(false);
+  const [toolsHover, setToolsHover] = useState(false);
+  const [backHover, setBackHover] = useState(false);
 
-  // const [num, setNum] = useState(1);
+  const puzzleMeHover = () => {
+    setMeHover((prev) => !prev);
+  };
 
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     setNum((prev) => (prev < 7 ? prev + 1 : prev));
-  //   }, 300);
-  // }, []);
+  const puzzleMeOffHover = () => {
+    setMeHover((prev) => !prev);
+  };
 
+  const puzzleFrontHover = () => {
+    setFrontHover((prev) => !prev);
+  };
+
+  const puzzleFrontOffHover = () => {
+    setFrontHover((prev) => !prev);
+  };
+
+  const puzzleToolsHover = () => {
+    setToolsHover((prev) => !prev);
+  };
+
+  const puzzleToolsOffHover = () => {
+    setToolsHover((prev) => !prev);
+  };
+
+  const puzzleBackHover = () => {
+    setBackHover((prev) => !prev);
+  };
+
+  const puzzleBackOffHover = () => {
+    setBackHover((prev) => !prev);
+  };
   return (
     <div className="parentContainer" id="about">
-      <div className="imgBio" id="scroll-item" ref={aboutContainer}>
-        <img className={bioVisible && "av-animate"} src={require(`./assets/avatarAnimation/6.png`)}></img>
-        <div className="bio-container" >
-          <div className={bioVisible ? "bio-box-animate" : "bio-box"}>
-            <h2 className="snapshot">A SNAPSHOT OF ME.</h2>
-            <h3>where I began</h3>
-            <p className={bioVisible ? "content-animate" : "content"}>
-              Originally my career started in marketing. I've worn many hats
-              from managing seven social media channels, repairing and
-              modernizing websites, project management for key marketing
-              initiatives, and serving as a sounding board for several internal
-              groups.
-            </p>
-            <h3>something was missing</h3>
-            <p className={bioVisible ? "content-animate" : "content"}>
-              After realizing that my favorite part of my marketing roles
-              revolved around the websites and indirect customer interaction
-              through UI, I knew my passion was somewhere else.
-            </p>
-            <h3>career change</h3>
-            <p className={bioVisible ? "content-animate" : "content"}>
-              In July of 2022, I began a 6-month Web Development program. While
-              learning the PERN stack, I also discovered my love for technical
-              problem-solving. I view all projects like a game, and solving each
-              bug as a level to complete. This mindset makes me addicted to
-              process and love what I do.
-            </p>
+      <div>
+        <h2>Skills</h2>
+        <h3>
+          "Coding is like a puzzle where pieces are constantly falling on the
+          floor." -Confucious{" "}
+        </h3>
+      </div>
+      <div>
+        <div className="imgBio" id="scroll-item" ref={aboutContainer}>
+          <div className="puzzle">
+            <div className="puzzle-top">
+              <img
+                src={require("./assets/puzzle/puzzle_frontend.png")}
+                onMouseEnter={puzzleFrontHover}
+                onMouseLeave={puzzleFrontOffHover}
+              ></img>
+              <img
+                className={meHover ? "puzzle-me-animation" : "puzzle-me"}
+                src={require("./assets/puzzle/puzzle_me.png")}
+                onMouseEnter={puzzleMeHover}
+                onMouseLeave={puzzleMeOffHover}
+              ></img>
+            </div>
+            <div className="puzzle-bottom">
+              <img
+                className="puzzle-tools"
+                src={require("./assets/puzzle/puzzle_tools.png")}
+                onMouseEnter={puzzleToolsHover}
+                onMouseLeave={puzzleToolsOffHover}
+              ></img>
+              <img
+                className="puzzle-backend"
+                src={require("./assets/puzzle/puzzle_backend.png")}
+                onMouseEnter={puzzleBackHover}
+                onMouseLeave={puzzleBackOffHover}
+              ></img>
+            </div>
+          </div>
+          <div className="bio">
+            {!meHover && !frontHover && !toolsHover && !backHover && <h3>Hover over a puzzle piece</h3>}
+            {meHover && (
+              <div className="under-text">
+                <h3>Why am I the missing piece?</h3>
+                <p>
+                  I am addicted to creation and solving problems. I am addicted
+                  to creation and solving problems.
+                </p>
+              </div>
+            )}
+            {frontHover && (
+              <div>
+                <h3>Front-end Skills</h3>
+                <div className="technologies">
+                  <div>
+                    <img
+                      alt="JavaScript Icon"
+                      src={require("./assets/tech_pngs/javascript.png")}
+                    ></img>
+                    <p>JavaScript</p>
+                  </div>
+                  <div>
+                    <img
+                      alt="React Icon"
+                      src={require("./assets/tech_pngs/react.png")}
+                    ></img>
+                    <p>React</p>
+                  </div>
+                  <div>
+                    <img
+                      alt="React Icon"
+                      src={require("./assets/tech_pngs/react.png")}
+                    ></img>
+                    <p>HTML5</p>
+                  </div>
+                  <div>
+                    <img
+                      alt="React Icon"
+                      src={require("./assets/tech_pngs/react.png")}
+                    ></img>
+                    <p>CSS3</p>
+                  </div>
+                </div>
+              </div>
+            )}
+            {toolsHover && (
+              <div>
+                <h3>Tools Experience</h3>
+                <div className="technologies">
+                  <div>
+                    <img
+                      alt="JavaScript Icon"
+                      src={require("./assets/tech_pngs/javascript.png")}
+                    ></img>
+                    <p>Git</p>
+                  </div>
+                  <div>
+                    <img
+                      alt="JavaScript Icon"
+                      src={require("./assets/tech_pngs/javascript.png")}
+                    ></img>
+                    <p>Netifly</p>
+                  </div>
+                  <div>
+                    <img
+                      alt="JavaScript Icon"
+                      src={require("./assets/tech_pngs/javascript.png")}
+                    ></img>
+                    <p>Heroku</p>
+                  </div>
+                  <div>
+                    <img
+                      alt="JavaScript Icon"
+                      src={require("./assets/tech_pngs/javascript.png")}
+                    ></img>
+                    <p>Webpack</p>
+                  </div>
+                  <div>
+                    <img
+                      alt="JavaScript Icon"
+                      src={require("./assets/tech_pngs/javascript.png")}
+                    ></img>
+                    <p>Postico</p>
+                  </div>
+                </div>
+              </div>
+            )}
+            {backHover && (
+              <div>
+                <h3>Back-end Skills</h3>
+                <div className="technologies">
+                  <div>
+                    <img
+                      alt="JavaScript Icon"
+                      src={require("./assets/tech_pngs/javascript.png")}
+                    ></img>
+                    <p>Node</p>
+                  </div>
+                  <div>
+                    <img
+                      alt="React Icon"
+                      src={require("./assets/tech_pngs/react.png")}
+                    ></img>
+                    <p>Express</p>
+                  </div>
+                  <div>
+                    <img
+                      alt="React Icon"
+                      src={require("./assets/tech_pngs/react.png")}
+                    ></img>
+                    <p>PSQL</p>
+                  </div>
+                  <div>
+                    <img
+                      alt="React Icon"
+                      src={require("./assets/tech_pngs/react.png")}
+                    ></img>
+                    <p>CSS3</p>
+                  </div>
+                  <div>
+                    <img
+                      alt="React Icon"
+                      src={require("./assets/tech_pngs/react.png")}
+                    ></img>
+                    <p>RESTful API</p>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
