fd23da4c5560d9b81d80a3649eb2eb88c07ea684
ekfman <ekfirstman@gmail.com>
2023-10-14 23:00:32 -0700

    navbar toggle menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import "./navbar.css";
 export function Navbar() {
-  //efefwe
+
+    const [navToggle, isNavToggle] = useState(false)
+
+    const menuClick = () => {
+        isNavToggle(prev => !prev)
+    }
+
   return (
     <nav className="navbar">
       <div className="navbar_container">
         <a href="/" id="navbar_logo">
           Erin Firstman
         </a>
-        <div className="navbar_toggle" id="mobile-menu">
+        <div onClick={menuClick} className={`navbar_toggle ${navToggle ? "is-active" : ""}`} id="mobile-menu">
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
         </div>
-        <ul className="navbar_menu">
+        <ul className={`navbar_menu ${navToggle ? "active" : ""}`}>
           <li className="navbar_item">
             <a href="/" className="navbar_links">
               Home
