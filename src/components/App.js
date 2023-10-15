957f3b5a281c54ef72a556ca89f758becd35cee7
ekfman <ekfirstman@gmail.com>
2023-10-14 17:09:49 -0700

    navbar css

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import "./app.css"
 const App = () => {
     return (
         <div>
-            <h1 className="hi">Hello</h1>
             <Navbar />
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,40 @@
-export function Navbar () {
-    //efefwe
-    return (
-        <nav className="navbar">
-            <div className="navbar_container"></div>
-            <a href="/" id="navbar_logo">Erin Firstman</a>
-            <div className="navbar_toggle" id="mobile-menu">
-                <span className="bar"></span>
-                <span className="bar"></span>
-                <span className="bar"></span>
-            </div>
-            <ul className="navbar_menu"></ul>
-                <li className="navbar_item">
-                <a href="/" className="navbar_links">
-                    Home
-                </a>
-                </li>
-                <li className="navbar_item">
-                <a href="/" className="navbar_links">
-                    About Me
-                </a>
-                </li>
-                <li className="navbar_item">
-                <a href="/" className="navbar_links">
-                    Projects
-                </a>
-                </li>
-                <li className="navbar_item">
-                <a href="/" className="navbar_links">
-                    Contact Me
-                </a>
-                </li>
-        </nav>
-    )
+import "./navbar.css";
+export function Navbar() {
+  //efefwe
+  return (
+    <nav className="navbar">
+      <div className="navbar_container">
+        <a href="/" id="navbar_logo">
+          Erin Firstman
+        </a>
+        <div className="navbar_toggle" id="mobile-menu">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+        <ul className="navbar_menu">
+          <li className="navbar_item">
+            <a href="/" className="navbar_links">
+              Home
+            </a>
+          </li>
+          <li className="navbar_item">
+            <a href="/" className="navbar_links">
+              About Me
+            </a>
+          </li>
+          <li className="navbar_item">
+            <a href="/" className="navbar_links">
+              Projects
+            </a>
+          </li>
+          <li className="navbar_item">
+            <a href="/" className="navbar_links">
+              Contact Me
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
