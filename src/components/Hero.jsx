9a0375a8d2d2e92dc6da962b7120f7c0fc42d2d0
ekfman <ekfirstman@gmail.com>
2023-10-16 21:32:23 -0700

    hero render animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import "./hero.css";
 import avatar from "./assets/avatar.png";
 import { Element } from "react-scroll";
+import { motion } from "framer-motion";
 
 export function Hero() {
   return (
@@ -8,13 +9,13 @@ export function Hero() {
     <div className="main">
       <div className="main_container">
         <div className="main_content">
-          <h1>Erin Firstman</h1>
-          <h2>Full-stack Software Engineer</h2>
-          <p>Addicted to turning ideas into creations</p>
+          <motion.h1 animate={{y: 0}} initial={{y: -200}} transition={{delay: .1}}>Erin Firstman</motion.h1>
+          <motion.h2 animate={{y: 0}} initial={{y: -100}} transition={{delay: .1}}>Full-stack Software Engineer</motion.h2>
+          <motion.p animate={{x: 0}} initial={{x: -100}} transition={{type:"tween", duration: .5}}>Addicted to turning ideas into <span className="creations">creations</span></motion.p>
         </div>
-        <div className="main_img_container">
+        <motion.div animate={{y: 0}} initial={{y: -200}} transition={{delay: .1}} className="main_img_container">
           <img src={avatar} alt="cartoonAvatar" id="main" />
-        </div>
+        </motion.div>
       </div>
     </div>
     </Element>
